f71f25f645c17a318e10789c8841bec80916380b
Jesli <jeslipriya07@gmail.com>
2026-07-17 20:27:01 +0530

    added

---
+++ src/numbers/GCD.java
@@ -0,0 +1,32 @@
+package numbers;
+
+import java.util.Scanner;
+
+public class GCD {
+
+    public static int gcd(int a, int b) {
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("GCD = " + gcd(a, b));
+
+        sc.close();
+    }
+}
